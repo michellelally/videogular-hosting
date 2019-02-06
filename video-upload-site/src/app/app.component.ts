b8f27d5a217dc29adbc525296bfb2455f4aa98f3
michellelallystudent <g00351333@gmit.ie>
2019-02-06 19:22:10 +0000

    multiple videos playing

--- video-upload-site/src/app/app.component.ts
+++ video-upload-site/src/app/app.component.ts
@@ -1,10 +1,43 @@
 import { Component } from '@angular/core';
 
+export interface Media {
+  title: string;
+  src: string;
+  type: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'video-upload-site';
+
+  playlist: Array<Media> = [
+    {
+      title: 'Pale Blue Dot',
+      src: 'http://static.videogular.com/assets/videos/videogular.mp4',
+      type: 'video/mp4'
+    },
+    {
+      title: 'Big Buck Bunny',
+      src: 'http://static.videogular.com/assets/videos/big_buck_bunny_720p_h264.mov',
+      type: 'video/mp4'
+    },
+    {
+      title: 'Elephants Dream',
+      src: 'http://static.videogular.com/assets/videos/elephants-dream.mp4',
+      type: 'video/mp4'
+    }
+  ];
+
+  currentIndex = 0;
+  currentItem: Media = this.playlist[this.currentIndex];
+
+  onClickPlaylistItem(item: Media, index:number) {
+    this.currentIndex = index;
+    this.currentItem = item;
+  }
+
 }
