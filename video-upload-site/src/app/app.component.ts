299c1f70fda75967c5e0795481180cd411928904
michellelallystudent <G00351333@gmit.ie>
2019-02-18 13:35:13 +0000

    upload and watch video components added

--- video-upload-site/src/app/app.component.ts
+++ video-upload-site/src/app/app.component.ts
@@ -1,11 +1,5 @@
 import { Component } from '@angular/core';
 
-export interface Media {
-  title: string;
-  src: string;
-  type: string;
-}
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -14,30 +8,6 @@ export interface Media {
 export class AppComponent {
   title = 'video-upload-site';
 
-  playlist: Array<Media> = [
-    {
-      title: 'Pale Blue Dot',
-      src: 'http://static.videogular.com/assets/videos/videogular.mp4',
-      type: 'video/mp4'
-    },
-    {
-      title: 'Big Buck Bunny',
-      src: 'http://static.videogular.com/assets/videos/big_buck_bunny_720p_h264.mov',
-      type: 'video/mp4'
-    },
-    {
-      title: 'Elephants Dream',
-      src: 'http://static.videogular.com/assets/videos/elephants-dream.mp4',
-      type: 'video/mp4'
-    }
-  ];
 
-  currentIndex = 0;
-  currentItem: Media = this.playlist[this.currentIndex];
-
-  onClickPlaylistItem(item: Media, index:number) {
-    this.currentIndex = index;
-    this.currentItem = item;
-  }
 
 }
