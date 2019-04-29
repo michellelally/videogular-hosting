6a99646b65b9d0676c92114f3141039582ccd72e
michellelally <shelley.lally@gmail.com>
2019-04-29 12:29:10 +0100

    final commit?

--- video-host-app/src/environments/environment.prod.ts
+++ video-host-app/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyDlSaImCK9tVSHqpqPlZ_WA-FZ5YmY_IHg",
+    authDomain: "gmit-video-host.firebaseapp.com",
+    databaseURL: "https://gmit-video-host.firebaseio.com",
+    projectId: "gmit-video-host",
+    storageBucket: "",
+    messagingSenderId: "305664337851"
+  }
+
 };
--- video-host-app/src/environments/environment.ts
+++ video-host-app/src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   firebaseConfig : {
     apiKey: "AIzaSyDlSaImCK9tVSHqpqPlZ_WA-FZ5YmY_IHg",
     authDomain: "gmit-video-host.firebaseapp.com",
